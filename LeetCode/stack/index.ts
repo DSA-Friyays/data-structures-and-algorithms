4e25f31892a7b9a8c3906a334ae9a9d9798548c5
Ibrahim <ibrahimbmohammed@gmail.com>
2023-05-28 14:42:09 +0100

    add: stack problems

---
+++ LeetCode/stack/index.ts
@@ -0,0 +1,34 @@
+// Valid Parentheses
+
+// Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+// An input string is valid if:
+
+// Open brackets must be closed by the same type of brackets.
+// Open brackets must be closed in the correct order.
+// Every close bracket has a corresponding open bracket of the same type.
+function isValid(s: string): boolean {
+  const pMap = {
+    "}": "{",
+    ")": "(",
+    "]": "[",
+  };
+  const stack = [];
+
+  for (let i = 0; i < s.length; i++) {
+    if (pMap[s[i]]) {
+      if (stack[stack.length - 1] === pMap[s[i]]) {
+        stack.pop();
+      } else {
+        return false;
+      }
+    } else {
+      stack.push(s[i]);
+    }
+  }
+  if (stack.length === 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
