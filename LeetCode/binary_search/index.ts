b3eebe505d5557844fa9495f9bf225098a19151e
Ibrahim <ibrahimbmohammed@gmail.com>
2023-06-03 11:05:23 +0100

    add: more  clean up

--- LeetCode/binary_search/index.ts
+++ LeetCode/binary_search/index.ts
@@ -26,52 +26,6 @@ function findMin(nums: number[]): number {
   return Math.min(result, nums[leftPnt]);
 }
 
-// Search in Rotated Sorted Array
-// There is an integer array nums sorted in ascending order (with distinct values).
-// Prior to being passed to your function, nums is possibly rotated at an unknown pivot index k (1 <= k < nums.length) such that the resulting array is [nums[k], nums[k+1], ..., nums[n-1], nums[0], nums[1], ..., nums[k-1]] (0-indexed). For example, [0,1,2,4,5,6,7] might be rotated at pivot index 3 and become [4,5,6,7,0,1,2].
-// Given the array nums after the possible rotation and an integer target, return the index of target if it is in nums, or -1 if it is not in nums.
-// You must write an algorithm with O(log n) runtime complexity.
-// Example 1:
-// Input: nums = [4,5,6,7,0,1,2], target = 0
-// Output: 4
-function search(nums: number[], target: number): number {
-  let leftPnt = 0;
-  let rightPnt = nums.length - 1;
-
-  while (leftPnt <= rightPnt) {
-    const mid = Math.floor((leftPnt + leftPnt) / 2);
-    // nums = [4,5,6,7,0,1,2], target = 7
-    if (nums[mid] === target) {
-      return mid;
-    }
-
-    // [4,5,6,7,0,1,2]
-    //[4,5,6,7]
-    // [0,1,2]
-
-    // [4,5,6,7,0,1,2,3,4]
-    //[0,1,2,3,4]
-
-    // target = 0
-    // im currently at the left
-    if (nums[mid] >= nums[leftPnt]) {
-      if (target > nums[mid] || target < nums[leftPnt]) {
-        leftPnt = mid + 1;
-      } else {
-        rightPnt = mid - 1;
-      }
-    } else {
-      if (target < nums[mid] || target > nums[rightPnt]) {
-        rightPnt = mid - 1;
-      } else {
-        leftPnt = mid + 1;
-      }
-    }
-  }
-
-  return -1;
-}
-
 //  Search in Rotated Sorted Array
 // There is an integer array nums sorted in ascending order (with distinct values).
 // Prior to being passed to your function, nums is possibly rotated at an unknown pivot index k (1 <= k < nums.length) such that the resulting array is [nums[k], nums[k+1], ..., nums[n-1], nums[0], nums[1], ..., nums[k-1]] (0-indexed). For example, [0,1,2,4,5,6,7] might be rotated at pivot index 3 and become [4,5,6,7,0,1,2].
